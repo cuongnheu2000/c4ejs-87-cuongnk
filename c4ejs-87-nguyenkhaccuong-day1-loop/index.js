10ed54f5f81f1c2454dd65944a394d439f761554
cuongnheu2000 <cuongnheu2000@gmail.com>
2020-06-21 22:59:58 +0700

    day1 mindx

---
+++ c4ejs-87-nguyenkhaccuong-day1-loop/index.js
@@ -0,0 +1,183 @@
+//ex3:
+// console.log('a.');
+// let x = 0;
+// for(x=0;x<7;x++){
+//     console.log(x);
+// }
+
+// console.log('b.');
+// let x = 0;
+// let n = prompt('nhập n: ');
+// for(x=0; x<n; x++){
+//     console.log(x);
+// }
+
+// console.log('c.');
+// let x = 3;
+// let n = prompt('nhập n: ');
+// for(x=3; 3< x <n; x++){
+//     console.log(x);
+// }
+
+// console.log('d.');
+// let x = prompt('nhập x: ');
+// let n = prompt('nhập n: ');
+// let y;
+// for(y=x; y<n; y++){
+//     console.log(y);
+// }
+
+// console.log('e.');
+// let n = Number(prompt('nhap n: '));
+// let c = Number(prompt('nhap c: '));
+// for(let i = c;i < n;i+=3){
+//     console.log(i);
+// }
+
+// console.log('f.');
+// let n = Number(prompt('nhap n: '));
+// let c = Number(prompt('nhap c: '));
+// let s = Number(prompt('nhap step s: '));
+// for(let i=c;i< n; i+=s){
+//     console.log(i);
+// }
+
+//ex4:
+// let n = prompt('nhập n: ');
+// let x = 1;
+// let i;
+// for( i=1; i <= n; i++){
+//     x=x*i;
+// }  
+// console.log(x);
+
+//exp5
+// let ask = prompt('bạn bao nhiêu tuổi? ');
+// if(ask> 14){
+//     alert('enjoy');
+// }
+// else{
+//     alert('bạn chưa đủ tuôi để enjoy');
+// }
+
+//ep6:
+//  let x = prompt('enter the number');
+//  if(x<5){
+//      alert('lower half of 9');
+//  }
+//  else if (x==5) {
+//      alert('1 nửa');
+//  }
+//  else{
+//      alert('higher half of 9');
+//  }
+
+//exp7:
+// let n = prompt('nhap n');
+// let x = prompt('nhap x');
+// if(x<(n/2)){
+//     alert(`${x} is in lower half of ${n}`);
+// }
+// else{
+//     alert(`${x} is in higher half of ${n}`);
+// }
+
+//ep8
+// let x = prompt('nhap so');
+// if (x%2 == 0){
+//     alert( ${x} là số chẵn `);
+// }
+// else{
+//     alert(`${x} la so le`);
+// }
+
+//ep10
+// let mass = prompt('nhập cân nặng: ');
+// let height = prompt('nhập chiều cao: ');
+// let x = height/100;
+// let BMI = mass/(x*x);
+// if(BMI < 16 ){
+//     alert(` your BMI is ${BMI} `);
+//     alert('Severely underweight');
+// }
+// else if(BMI >= 16 && BMI < 18.5){
+//     alert(` your BMI is ${BMI} `);
+//     alert('underweight');
+// }
+// else if(BMI >= 18.5 && BMI < 25 ){
+//     alert(` your BMI is ${BMI} `);
+//     alert('normal');
+// }
+// else if(BMI >= 25 && BMI < 30){
+//     alert(` your BMI is ${BMI} `);
+//     alert('overweight');
+// }
+// else{
+//     alert(` your BMI is ${BMI} `);
+//     alert('obese');
+//}
+
+//ep9:
+// console.log('a.');
+// for(let i=0;i<3;i++){
+//     console.log('L');
+// }
+// for(let i=0;i<3;i++){
+//     console.log('H');
+// }
+
+// console.log('b.');
+// let n=prompt('nhap n: ');
+// for(let i=0;i<Math.floor(n/2);i++){
+//     console.log('L');
+// }
+// for(let i=0;i<Math.ceil(n/2);i++){
+//     console.log('H');
+// }
+
+// console.log('c.');
+// for(let i=0;i<4;i++){
+//     console.log(0);
+//     console.log(1);
+// }
+
+// console.log('d.');
+// let n=prompt('nhap n: ');
+// for(let i=0;i<n;i++){
+//     if(i%2==0){
+//         console.log(0);
+//     } else{
+//         console.log(1);
+//     }
+// }
+
+//ep11:
+// function ngugiac(){
+//   for(let i=1;i<=5;i++){
+//     lt(72)
+//     fd(60)
+//   }
+// }ngugiac()
+
+// function tamgiac(){
+//   for(let i=1;i<=3;i++){
+//     lt(120)
+//     fd(60)
+//   }
+// }tamgiac()
+
+// function lucgiac(){
+//   for(let i=1;i<=5;i++){
+//     lt(60)
+//     fd(60)
+//   }
+// }lucgiac()
+
+// function hinhvuong() {
+//   for (var i=1; i <= 4; i++) {
+//     fd(50)
+//     lt(90)
+//   }
+// }
+// hinhvuong()
+
---
+++ c4ejs-87-nguyenkhaccuong-day1/index.js
@@ -0,0 +1,59 @@
+//ex1:
+// console.log(1);
+// let x = 23a;
+// console.log(x);
+// let y = @heello;
+// console.log(y);
+// let z = break;
+// console.log(z);
+
+// console.log(2);
+// let x = 'cuong';
+// console.log(typeof x);
+
+//ex3:
+// console.log('a.');
+// let message = 'coding is great';
+// console.log(message);
+// console.log('b.');
+// let studentcount = 0;
+// console.log("studentcount: "  +studentcount);
+
+//ex4:
+// console.log('a.');
+// message = 'Coding might not be easy, but still great';
+// console.log(message);
+// console.log('b.');
+// studentcount = 16;
+// console.log("studentcount: "  +studentcount);
+// console.log('c.');
+// let x = message.toLowerCase();
+// console.log(x);
+// console.log('d.');
+// studentcount++;
+// console.log(studentcount);
+
+//exp5:
+//alert('you look so beautiful');
+
+//exp6:
+// let name = prompt('xin chào, cho biết tên của bạn: ');
+// alert("hi, " + name) ;
+
+//exp7:
+// let lastname = prompt('nhập tên của bạn: ');
+// let firstname = prompt('nhập họ của bạn: ');
+// alert("xin chào, "+' ' +lastname  +' ' +firstname);
+
+//exp8:
+// let x = prompt(' cạnh hình vuông = ');
+// alert('S hình vuông = ' +x*x);
+
+//exp9:
+// let x = prompt(' bán kính R = ');
+// alert('S hình tròn = ' +x*x*3.14);
+
+//exp10:
+// let x = prompt('nhap gia tri do C: ');
+// let y = x*1.8 + 32;
+// alert('gia tri do F la: ' +y);
